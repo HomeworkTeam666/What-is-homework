28f83994d9f6d3c1c04846caca9f3775149a1393
pp2468825 <pp2468825@126.com>
2019-11-29 15:53:11 +0800

    自动查询
    标题界面初始化自动查询

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,18 +5,40 @@ Page({
     tabs: ["选项", "选项", "选项", "选项", "选项", "选项", "选项", "选项", "选项", "选项"],
     activeIndex: 0,
 
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
     duration: 1000,
+    array:'',
 
     sort: 1
   },
+
+    onLoad: function (options) {
+        
+        const db = wx.cloud.database()
+
+        db.collection('Courses').where({
+                _openid: this.data.openid
+        }).get({
+                success: res => {
+                    this.setData
+                        ({
+                            array: res.data
+                        })
+                    console.log('[数据库] [查询记录] 成功: ', res)
+                    return res
+                },
+                fail: err => {
+                    wx.showToast
+                        ({
+                            icon: 'none',
+                            title: '查询记录失败'
+                        })
+                    console.error('[数据库] [查询记录] 失败：', err)
+                }
+            })
+    },
 
   tabClick: function (e) {
     this.setData({
@@ -52,7 +74,7 @@ Page({
   },
   todetail(){
     wx.switchTab({
-      url: '../Mine/index',
+      url: '../detail/index',
     })
 
   }
